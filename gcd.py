6c6fa577d2690c850fe88f33f794c2fc22010530
Vandan99 <81004735+Vandan99@users.noreply.github.com>
2021-03-20 16:21:29 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,15 @@
+# Python code to find gcd
+# method to compute gcd ( recursion ) 
+
+
+def gcd(a, b): 
+	if(b == 0): 
+		return a 
+	else: 
+		return gcd(b, a % b) 
+
+a = 60
+b = 48
+
+print("The gcd of 60 and 48 is : ", end="") 
+print(gcd(60, 48)) 
